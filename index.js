ae5989b916f13d7c94e687a263d21109ed7de8a4
Filip Chomczynski <f.chomczynski@gmail.com>
2023-01-15 11:51:09 +0100

    github

--- index.js
+++ index.js
@@ -7,18 +7,31 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+let urls = [];
+
+app.get('/', function (req, res) {
+	res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function (req, res) {
+	const url = req.body.url;
+	if (!urls.includes(url)) {
+		urls.push(url);
+	}
+
+	return res.json({
+		orginalUrl: url,
+		shortUrl: urls.indexOf(url),
+	});
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+app.get('/api/shorturl/:short_url', function (req, res) {
+	res.redirect(urls[req.params.short_url]);
+});
+
+app.listen(port, function () {
+	console.log(`Listening on port ${port}`);
 });
